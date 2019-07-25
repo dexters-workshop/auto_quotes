afe61cb17fb46ffb62f42a3de3747d70d3b56d40
Jason Dexter <jddexter@ucdavis.edu>
2019-07-24 17:34:10 -0700

    cleaned up and commented code

--- send_quote_at_scheduled_time.R
+++ send_quote_at_scheduled_time.R
@@ -1,12 +1,12 @@
-# CHRON JOB FOR SENDING HABIT RELATED QUOTES
+# AUTOMATED SMS BY PULLING CONTACT INFO FROM GOOGLE-SHEETS
 
 # 0.0 HIGH-LEVEL SCRIPT OVERVIEW ----
-# 1) Pulls Member Contact Details from Google Sheets
-# 2) Selects Habit Related Quote from table
-# 3) Builds Message for SMS as Text
-# 4) Sends SMS Quote/Message to each Group Member
-        # Planning to have a Mon messsage + Fri message
-        # Once i get further along in setting cron-job
+    # 1) Pulls Member Contact Details from Google Sheets
+    # 2) Selects Habit/Systems Related Quote from data-table
+    # 3) Builds Message for SMS (as Text)
+    # 4) Sends SMS Quote/Message to each Group Member
+
+# Current settings for our Monday/Friday Sends to group
 
 # Load libraries
 library(tidyverse)     # workhorse package
@@ -16,13 +16,6 @@ library(twilio)        # interface to Twilio API for R
 library(cronR)         # for managing cron jobs
 library(lubridate)     # for working with dates
 
-
-# Variable for Controlling which message to send
-wk_day_chr <- wday(today(), label = T, abbr = F)
-    # this will be for chron-job later on (alpha-stage)
-    # SMS messages will go out Mon + Fri
-    # Pulling day from today() will allow for controlling  script flow:
-        # which message to send: week or weekend message.
 
 # 1.0 Get Member Phone Numbers ----
 
@@ -41,89 +34,110 @@ app4that_gs_obj <- gs_title("app4that_test")
 member_info_tbl <- app4that_gs_obj %>% 
     
     # Specify worksheet to pull data from
-    gs_read_csv(ws = "Member Contact Info", 
+    gs_read_csv(ws   = "Member Contact Info", 
                 skip = 2)
 
 # 1.3 Cleanse Phone # Data ----
 
-# Clean up Phone # Data
+# Clean up Member Phone # Data
 cleaned_phone_numbers_tbl <- member_info_tbl %>% 
     
     # Get column w/member phone #s
     select(Number) %>% 
     
-    # Cleanse phone #s - remove non-numerical values
+    # Cleanse phone #s in 'Number' Column: Remove non-numerical characters
     mutate(Number = str_replace_all(
-        string = Number,
-        pattern = "[^[:alnum:]]",
-        replacement = "")) %>% 
+            string      = Number,
+            pattern     = "[^[:alnum:]]",
+            replacement = "")
+           ) %>% 
     
     # Convert phone #s from character to numeric
     mutate_if(is.character, as.numeric) 
 
-# Save Phone #s in List
+# Pull + Save Phone #s in List
 cleaned_phone_numbers_list <- cleaned_phone_numbers_tbl %>%
     pull(Number)
 
 
- # 2.0 Pull Random Quote ----
+# 2.0 Pull Random Quote ----
 
 # Read in quotes table
 awesome_quotes_tbl <- read_excel("01_Data/awesome_quotes.xlsx")
 
-# Randomly choose an awesome quote (draft stage)
+# Randomly Select Awesome Quote + Prep for SMS
 awesome_quote_chr <- awesome_quotes_tbl %>% 
     
-    # Select Random Row in Quotes Table
+    # Select Random Row in Quotes Data-Table
     sample_n(size = 1) %>% 
     
-    # Select Quote + Author and combine them in new column
+    # Select Quote + Author Columns
     select(quote, author) %>% 
+    
+    # Formatting: combine quote then author
     mutate(quote_with_author = str_glue(
         "\"{quote}\" - {author}"
     )) %>% 
+    
+    # Select + Pull formatted quote as character/string
     select(quote_with_author) %>% 
     pull(quote_with_author)
 
 
 # 3.0 Build Message for Send ----
 
-# 3.1 Setup Message Prefix/Suffix ----
+# For now, we are sending a Monday + Friday message
 
-# Setup Monday Message Prefix/Suffix
-prefix_mon_chr <- "BETA-TESTING\n\nWeekly Quote from the App4That Group: \n\n"
-suffix_mon_chr <- "\n\nHave a Great Week <><"
+# 3.1 Controlling flow w/Weekday Variable ----
 
-# Setup Friday Message Prefix/Suffix
-prefix_fri_chr <- "BETA-TESTING\n\nHappy Friday App4That Group Members: \n\n"
-suffix_fri_chr <- "\n\nHave an Awesome Weekend <><"
+# Pull weekday from today()
+wk_day_chr <- wday(today(), label = TRUE) %>% str_to_lower()
+# wk_day_chr <- "mon"    # uncomment for testing
 
-# 3.2 Combine Pieces for SMS Message ----
 
-# Build Monday Message
-message_to_group <- str_glue(
-    "{prefix_fri_chr}{awesome_quote_chr}{suffix_fri_chr}")
+# 3.2 Source Function + Build Message ----
 
-# Build Friday Message
-message_to_group <- str_glue(
-    "{prefix_fri_chr}{awesome_quote_chr}{suffix_fri_chr}")
+# Source function from build_message.R
+source("02_Scripts/build_message.R")
+
+# Call function + build message
+message_to_group <- build_message(wk_day_chr)
+
 
 # 4.0 Send Quote as Text via SMS ----
+
+# 4.1 Get + Set Environmental Variables
 
 # Set twilio account SID and token as environmental variables
 Sys.setenv(TWILIO_SID   = Sys.getenv("my_twilio_sid"))
 Sys.setenv(TWILIO_TOKEN = Sys.getenv("my_twilio_token"))
 
 # Store the numbers in some variables
-my_phone_number  <- Sys.getenv("my_phone_number")
 my_twilio_number <- Sys.getenv("my_twilio_number")
+# my_phone_number  <- Sys.getenv("my_phone_number")
 
-# Send Message via SMS
 
-# Loop to Send Each Member the Message
+# 4.2 Utilzie Twilio to Send Message via SMS ----
+
+# Iteration Method 01: Iterate using Purr + map()
+
+# Use anonymous function that iterates over # list
+cleaned_phone_numbers_list %>% 
+    
+    # Call twilio function to send message
+    map(~ tw_send_message(
+        from = my_twilio_number,
+        to   = ., 
+        body = message_to_group
+    ))
+
+
+# Iteration Method 02: Iterate using a base-R for-loop
+
+# Use for-Loop to iterate over Member phone # list
 for (member in 1:length(cleaned_phone_numbers_list)) {
     
-    # Loop over Member phone #s and send Message
+    # Call twilio function to send message
     tw_send_message(from = my_twilio_number,
                     to   = cleaned_phone_numbers_list[member], 
                     body = message_to_group)
